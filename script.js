b68ad83c0eb10389fea0a97cb01cb00e5434d67d
unknown <justinemalabuyoc1@gmail.com>
2024-02-14 16:01:04 +0800

    updates100%

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ burgir.addEventListener("click", () => {
     burgir.classList.toggle("open");
     headerNav.classList.toggle("open");
 })
+const header = document.querySelector(".header");
+window.console.log(scrollY);
+window.addEventListener("scroll", () => {
+  if (scrollY >= 100) {
+    header.classList.add("open");
+  } else {
+    header.classList.remove("open");
+  }
+}); 
